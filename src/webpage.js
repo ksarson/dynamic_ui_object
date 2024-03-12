deab7c0addf9f4b71f39ab484c94693386b6a18a
ksarson <37591778+ksarson@users.noreply.github.com>
2024-03-12 18:04:33 -0400

    Image Carousel

--- src/webpage.js
+++ src/webpage.js
@@ -17,4 +17,27 @@ document.querySelector('.navitem').addEventListener('click', (event) => {
   }
 });
 
+const carousel = document.querySelector('.carousel');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+
+let currentIndex = 0;
+const imageShare = 100 / carousel.children.length;
+
+function updateCarousel() {
+  const offset = -currentIndex * imageShare;
+  carousel.style.transform = `translateX(${offset + imageShare}%)`;
+}
+
+nextBtn.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % carousel.children.length;
+  updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+  currentIndex =
+    (currentIndex - 1 + carousel.children.length) % carousel.children.length;
+  updateCarousel();
+});
+
 export default initializeWebsite;
